537097e56bae1d68cb38ae650696254f79f5ce6d
JunRot <hoangquan091197@gmail.com>
2025-03-15 00:46:07 +0700

    update homework part3-b

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('dist'))
 
 // Custom token to log request body for POST requests
 morgan.token('post-data', (req) => {
